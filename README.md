93d7b13b7f00e06ee5dc0835d4df85910d81ae75
Tristan Sloughter <tristan@heroku.com>
2013-08-27 11:55:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Node.js Websocket Test
+
+Uses the [ws library](http://einaros.github.io/ws/).
+
+# Running
+
+``` bash
+$ npm install
+$ PORT=8080 foreman start
+```
+
+# Running on Heroku
+
+``` bash
+$ heroku create
+$ heroku sudo labs:enable websockets
+$ git push heroku master
+$ heroku open
+```
---
+++ server.js
@@ -0,0 +1,27 @@
+var WebSocketServer = require('ws').Server
+  , http = require('http')
+  , express = require('express')
+  , app = express()
+  , port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/'));
+
+var server = http.createServer(app);
+server.listen(port);
+
+console.log('http server listening on %d', port);
+
+var wss = new WebSocketServer({server: server});
+console.log('websocket server created');
+wss.on('connection', function(ws) {
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {  });
+    }, 1000);
+
+    console.log('websocket connection open');
+
+    ws.on('close', function() {
+        console.log('websocket connection close');
+        clearInterval(id);
+    });
+});
