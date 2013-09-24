9fff462d156653a450b43686fcab06be4b3f4aa1
Tristan Sloughter <t@crashfast.com>
2013-09-24 14:14:16 -0500

    remove old client js file

--- client.js
+++
@@ -1,9 +0,0 @@
-var server = process.env.SERVER || "localhost:5000"
-var WebSocket = require('ws');
-var ws = new WebSocket('ws://' + server + '/path');
-ws.on('open', function() {
-    ws.send('client sends something via websockets');
-});
-ws.on('message', function(data, flags) {
-    console.log('received: %s', data);
-});
